b866b9840c59576ef24c117c48c5751b667be072
Neethu <neethum_mca22-24@macfast.ac.in>
2023-11-22 18:58:56 +0530

    first commit

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,23 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
-constructor(private api:ApiService){ 
+constructor(private api:ApiService){}
+todos:any;
+inputAdd!:string;
+title:string='';
+ngOnInit(){
   this.api.getTodo().subscribe((data:any)=>{
     this.todos=data
   })
-}
-todos:any;
-ngOnInit(){
 
 }
 
+del(data:any)
+{
+this.todos=this.todos.filter((item:{id:any;})=>item.id!==data.id);
 }
+add(){
+  if(this,this.title.trim()!==''){
+    const news={title:this.title,finished:false};
+    this.todos.push(news);
+    this.title='';
+  }
+}
+
+}
+
+
